5e7d2467bd36cfeb2c412a9db620a5b2d585b072
webroute <enmatushin@gmail.com>
2011-10-18 23:57:16 +0400

    fix some problems with icmp protocol counting

--- scripts/ipcad2sql.py
+++ scripts/ipcad2sql.py
@@ -3,6 +3,12 @@ import MySQLdb
 db= MySQLdb.connect(host='localhost', user='webroute', passwd='wbr', db='webroute')
 cursor= db.cursor()
 tfile = open('/var/traf/tmp', 'r')
+a = 0
 for line in tfile:
-    cursor.execute(line)
+    try:
+        cursor.execute(line)
+        a = a + 1
+    except:
+        pass
 tfile.close()
+print a
